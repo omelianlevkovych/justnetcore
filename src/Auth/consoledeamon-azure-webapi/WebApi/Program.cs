7366343c9156794da488c384ad6b460d9b778cdc
OmelianLevkovych <omelianai@gmail.com>
2022-03-14 23:49:39 +0200

    added simple api to play with azure active directory

---
+++ src/Auth/consoledeamon-azure-webapi/WebApi/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opt =>
+    {
+        // the API
+        opt.Audience = builder.Configuration["AAD_RESOURCEID"];
+        // the active directory is our authority
+        opt.Authority = $"{builder.Configuration["AAD_Instance"]}{builder.Configuration["AAD_TENANTID"]}";
+    });
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
